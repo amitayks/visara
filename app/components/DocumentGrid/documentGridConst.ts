20f050ee17e0c097bb543826b11b8790bc8a3fe3
amitay keisar <amiteyk3@gmail.com>
2025-09-20 21:28:31 +0300

    4 columns cahnge

--- app/components/DocumentGrid/documentGridConst.ts
+++ app/components/DocumentGrid/documentGridConst.ts
@@ -1,6 +1,6 @@
 import { SCREEN_WIDTH } from "../../../constants/dimensions";
 
-export const COLUMNS = 3;
+export const COLUMNS = 4;
 export const SPACING = 8; // Reduced spacing between columns
 export const CONTAINER_PADDING = 8;
 // Calculate item width accounting for container padding and inter-item spacing
