cc6a64461d329eaca1269ff21230b20c0b648a6c
amitay keisar <amiteyk3@gmail.com>
2025-09-20 21:28:58 +0300

    4 columns change

--- app/components/DocumentGrid/documentGridConst.ts
+++ app/components/DocumentGrid/documentGridConst.ts
@@ -3,7 +3,7 @@ import { SCREEN_WIDTH } from "../../../constants/dimensions";
 export const COLUMNS = 4;
 export const SPACING = 8; // Reduced spacing between columns
 export const CONTAINER_PADDING = 8;
-// Calculate item width accounting for container padding and inter-item spacing
+
 export const ITEM_WIDTH =
 	(SCREEN_WIDTH - CONTAINER_PADDING - SPACING * 1.2) / COLUMNS;
 export const ITEM_HEIGHT = 1.4 * ITEM_WIDTH; // Assuming a 5:7 aspect ratio for document thumbnails
